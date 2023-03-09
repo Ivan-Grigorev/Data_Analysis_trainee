47d905f61f4b65dbb137ec0414dd33a1c18791ae
Ivan Grigorev <iv.st.grigorev@gmail.com>
2023-03-09 21:55:27 +0100

    Modified functions. Deleted handler.py and replaced all functions to NYCTaxiAnalysis class.

--- NYC_taxi/nyc_taxi_analysis.py
+++ NYC_taxi/nyc_taxi_analysis.py
@@ -1,14 +1,16 @@
 import pandas as pd
+import numpy as np
 
-from handler import get_addresses, get_rides_of_the_day
+from geopy import Nominatim
 
 pd.set_option("display.max_columns", 30)
 pd.set_option("display.width", 1000)
 
 
 class NYCTaxiAnalysis:
-    def __init__(self, dataset, lines=None, date_from=None, date_to=None):
-        self.data = pd.read_csv(dataset).head(lines)
+    def __init__(self, dataset, date_from: str, date_to: str, lines=None):
+        # Filter dataset by date
+        self.data = pd.read_csv(dataset).query("@date_from <= pickup_datetime <= @date_to").head(lines)
 
         self.morning_rides = None
         self.afternoon_rides = None
@@ -24,55 +26,56 @@ class NYCTaxiAnalysis:
 
     def rides_of_the_day(self):
         # This function analyze taxi rides based on time of day: morning, afternoon, evening, late night/early morning
-        self.morning_rides = get_rides_of_the_day(
-            data=self.data,
-            start_date=self.date_from,
-            start_time="06:00:00",
-            end_date=self.date_to,
-            end_time="12:00:00",
-        )
-        self.afternoon_rides = get_rides_of_the_day(
-            data=self.data,
-            start_date=self.date_from,
-            start_time="12:00:00",
-            end_date=self.date_to,
-            end_time="18:00:00",
-        )
-        self.evening_rides = get_rides_of_the_day(
-            data=self.data,
-            start_date=self.date_from,
-            start_time="18:00:00",
-            end_date=self.date_to,
-            end_time="23:59:59",
-        )
-        self.night_rides = get_rides_of_the_day(
-            data=self.data,
-            start_date=self.date_from,
-            start_time="00:00:00",
-            end_date=self.date_to,
-            end_time="06:00:00",
-        )
+        try:
+            # Convert pickup_datetime column to datetime dtypes
+            self.data["pickup_datetime"] = pd.to_datetime(self.data["pickup_datetime"], format="%Y-%m-%d %H:%M:%S")
 
-        return f"The total rides from time period 6:00 a.m.(06:00:00) to 12:00 p.m. (12:00:00) and date period from " \
-               f"{self.date_from} to {self.date_to} is {len(self.morning_rides)}\n" \
-               f"The total rides from time period 12:00 p.m. (12:00:00) to 6:00 p.m. (18:00:00) and date period from " \
-               f"{self.date_from} to {self.date_to} is {len(self.afternoon_rides)}\n" \
-               f"The total rides from time period 6:00 p.m. (18:00:00) to 12 a.m. (00:00:00) and date period from " \
-               f"{self.date_from} to {self.date_to} is {len(self.evening_rides)}\n" \
-               f"The total rides from time period 12:00 a.m. (00:00:00) to 6:00 a.m. (06:00:00) and date period from " \
-               f"{self.date_from} to {self.date_to} is {len(self.night_rides)}"
+            # Calculate the number of rides for each time period
+            self.morning_rides = self.data.loc[
+                (self.data["pickup_datetime"].dt.time >= pd.to_datetime('06:00:00').time()) &
+                (self.data["pickup_datetime"].dt.time <= pd.to_datetime('12:00:00').time())
+            ].sort_values(by="pickup_datetime", ascending=True)
 
-        # return self.morning_rides, self.afternoon_rides, self.evening_rides, self.night_rides
+            self.afternoon_rides = self.data.loc[
+                (self.data["pickup_datetime"].dt.time >= pd.to_datetime('12:00:00').time()) &
+                (self.data["pickup_datetime"].dt.time <= pd.to_datetime('18:00:00').time())
+            ].sort_values(by="pickup_datetime", ascending=True)
+
+            self.evening_rides = self.data.loc[
+                (self.data["pickup_datetime"].dt.time >= pd.to_datetime('18:00:00').time()) &
+                (self.data["pickup_datetime"].dt.time <= pd.to_datetime('23:59:59').time())
+            ].sort_values(by="pickup_datetime", ascending=True)
+
+            self.night_rides = self.data.loc[
+                (self.data["pickup_datetime"].dt.time >= pd.to_datetime('00:00:00').time()) &
+                (self.data["pickup_datetime"].dt.time <= pd.to_datetime('06:00:00').time())
+            ].sort_values(by="pickup_datetime", ascending=True)
+
+            return f"The total rides from {self.date_from} to {self.date_to}, there were:\n" \
+                   f"\t{len(self.morning_rides)} rides from 6:00 a.m. (06:00:00) to 12:00 p.m. (12:00:00);\n" \
+                   f"\t{len(self.afternoon_rides)} rides from 12:00 p.m. (12:00:00) to 6:00 p.m. (18:00:00);\n" \
+                   f"\t{len(self.evening_rides)} rides from 6:00 p.m. (18:00:00) to 12 a.m. (00:00:00);\n" \
+                   f"\t{len(self.night_rides)} rides from 12:00 a.m. (00:00:00) to 6:00 a.m. (06:00:00)"
+
+        except KeyError as err:
+            # Display error message in error key
+            return f"WARNING! The function {NYCTaxiAnalysis.rides_of_the_day.__name__}() error - {err}"
 
     def pickup_dropoff_addresses(self):
         # This function get all pickup and dropoff addresses by longitude/latitude and add it to dataframe
-        self.pickup_coordinates = list(zip(self.data["pickup_latitude"].values,
-                                           self.data["pickup_longitude"].values, ))
-        self.dropoff_coordinates = list(zip(self.data["dropoff_latitude"].values,
-                                            self.data["dropoff_longitude"].values, ))
+        pickup_lat = self.data["pickup_latitude"].values
+        pickup_lon = self.data["pickup_longitude"].values
+        dropoff_lat = self.data["dropoff_latitude"].values
+        dropoff_lon = self.data["dropoff_longitude"].values
 
-        self.pickup_address, self.dropoff_address = get_addresses(self.pickup_coordinates,
-                                                                  self.dropoff_coordinates)
+        # Stack the latitude/longitude arrays horizontally and transpose to create tuples
+        self.pickup_coordinates = np.vstack((pickup_lat, pickup_lon)).T
+        self.dropoff_coordinates = np.vstack((dropoff_lat, dropoff_lon)).T
+
+        geolocator = Nominatim(user_agent="NYC_taxi")
+
+        self.pickup_address = [geolocator.reverse(coord).address for coord in self.pickup_coordinates]
+        self.dropoff_address = [geolocator.reverse(coord).address for coord in self.dropoff_coordinates]
 
         self.data["pickup_address"] = self.pickup_address
         self.data["dropoff_address"] = self.dropoff_address
@@ -80,14 +83,16 @@ class NYCTaxiAnalysis:
         return self.data
 
     def analysis_visualization(self):
-        pass
+        return self.data
 
 
 if __name__ == "__main__":
     nyc_taxi_analysis = NYCTaxiAnalysis(dataset="../CSV/nyc_taxi_trip_duration.csv",
-                                        date_from="2016-01-01",
-                                        date_to="2016-01-30", )
+                                        date_from='2016-01-01',
+                                        date_to='2016-01-30',
+                                        lines=10)
 
-    # nyc_taxi_analysis.pickup_dropoff_addresses()
-    print(nyc_taxi_analysis.rides_of_the_day())
-    # print(nyc_taxi_analysis.analysis_visualization())
+    # print(nyc_taxi_analysis.rides_of_the_day())
+    # print(nyc_taxi_analysis.pickup_dropoff_addresses())
+
+    print(nyc_taxi_analysis.analysis_visualization())
