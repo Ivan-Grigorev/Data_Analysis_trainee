956fe73dbf9246f1de71d26d2bb59405662246e6
Ivan Grigorev <iv.st.grigorev@gmail.com>
2023-03-09 21:58:00 +0100

    Modified all tests. Created TestNYCTaxiAnalysis

--- NYC_taxi/tests/test_taxi_analysis.py
+++ NYC_taxi/tests/test_taxi_analysis.py
@@ -1,88 +1,67 @@
 import unittest
 import pandas as pd
 
-from NYC_taxi.handler import get_addresses, get_rides_of_the_day
+from NYC_taxi.nyc_taxi_analysis import NYCTaxiAnalysis
 
 
 class TestGetRidesOfTheDay(unittest.TestCase):
+
     def setUp(self):
-        # Define sample data for testing
-        data = {
-            "pickup_datetime": pd.date_range(
-                start="2016-01-01 00:00:00", end="2016-01-31 23:59:59", freq="s"
-            )
-        }
-        self.df = pd.DataFrame(data)
+        # Create sample dataframe
+        self.data = pd.DataFrame({
+            "pickup_datetime": ["2022-01-01 06:00:00", "2022-01-01 07:00:00", "2022-01-01 13:00:00",
+                                "2022-01-01 19:00:00", "2022-01-02 01:00:00"],
+            "pickup_latitude": [40.737431, 40.731610, 40.717842, 40.734610, 40.737431],
+            "pickup_longitude": [-73.984077, -73.936242, -74.005508, -73.939242, -73.984077],
+            "dropoff_latitude": [40.749981, 40.741610, 40.735226, 40.744610, 40.749981],
+            "dropoff_longitude": [-73.991409, -73.936242, -74.003281, -73.939242, -73.991409]
 
-    def test_get_rides_of_the_day(self):
-        # Define expected output
-        start_datetime = pd.to_datetime("2016-01-01 06:00:00")
-        end_datetime = pd.to_datetime("2016-01-30 12:00:00")
+        })
 
-        # Filter dataframe
-        filtered_df = get_rides_of_the_day(
-            self.df,
-            start_date="2016-01-01",
-            start_time="06:00:00",
-            end_date="2016-01-30",
-            end_time="12:00:00",
-        )
+        # Create CSV file with test data
+        self.file_ = "test_data.csv"
+        self.data.to_csv(self.file_, index=False)
 
-        # Check that all values in pickup_datetime are within the expected range
-        self.assertTrue(filtered_df["pickup_datetime"].min() >= start_datetime)
-        self.assertTrue(filtered_df["pickup_datetime"].max() <= end_datetime)
+        # Create instance of NYCTaxiAnalysis class
+        self.analysis = NYCTaxiAnalysis(dataset=self.file_, lines=5, date_from="2022-01-01", date_to="2022-01-02")
 
-    def test_get_rides_of_the_day_invalid_input(self):
-        # Check if function raises ValueError when inputs are invalid
-        with self.assertRaises(ValueError):
-            get_rides_of_the_day(
-                self.df, "2016-01-01", "2016-01-30", "12:00:00", "06:00:00"
-            )
-            get_rides_of_the_day(
-                self.df, "2016-01-01", "2016-01-01", "06:00:00", "12:00:00"
-            )
-            get_rides_of_the_day(
-                self.df, "2016-01-30", "2016-01-01", "06:00:00", "12:00:00"
-            )
-            get_rides_of_the_day(
-                self.df, "2016-01-01", "2016-01-30", "00:00:00", "24:00:00"
-            )
+    def test_rides_of_the_day(self):
+        # Ensure the total number of rides is correct
+        self.assertEqual(self.analysis.rides_of_the_day(),
+                         "The total rides from 2022-01-01 to 2022-01-02, there were:\n"
+                         "\t2 rides from 6:00 a.m. (06:00:00) to 12:00 p.m. (12:00:00);\n"
+                         "\t1 rides from 12:00 p.m. (12:00:00) to 6:00 p.m. (18:00:00);\n"
+                         "\t1 rides from 6:00 p.m. (18:00:00) to 12 a.m. (00:00:00);\n"
+                         "\t1 rides from 12:00 a.m. (00:00:00) to 6:00 a.m. (06:00:00)")
 
+        # Ensure the output includes the expected number of rides for each time period
+        self.assertIn("2 rides from 6:00 a.m. (06:00:00) to 12:00 p.m. (12:00:00)", self.analysis.rides_of_the_day())
+        self.assertIn("1 rides from 12:00 p.m. (12:00:00) to 6:00 p.m. (18:00:00)", self.analysis.rides_of_the_day())
+        self.assertIn("1 rides from 6:00 p.m. (18:00:00) to 12 a.m. (00:00:00)", self.analysis.rides_of_the_day())
+        self.assertIn("1 rides from 12:00 a.m. (00:00:00) to 6:00 a.m. (06:00:00)", self.analysis.rides_of_the_day())
 
-class TestGetAddress(unittest.TestCase):
-    def test_get_addresses_attributes(self):
-        # Test that all attributes are correct
-        pickup_coordinates = [("40.77887344360352", "-73.95391845703125")]
-        dropoff_coordinates = [("40.77116394042969", "-73.96387481689453")]
-        pickup_address, dropoff_address = get_addresses(
-            pickup_coordinates, dropoff_coordinates
-        )
+    def test_pickup_dropoff_addresses(self):
+        # Ensure the pickup and dropoff addresses are correctly retrieved
+        addresses = self.analysis.pickup_dropoff_addresses()
 
-        self.assertEqual(len(pickup_coordinates), len(dropoff_coordinates))
-        self.assertTrue(type(pickup_coordinates) is list)
-        self.assertTrue(type(dropoff_coordinates) is list)
+        self.assertIsNotNone(self.analysis.pickup_coordinates)
+        self.assertIsNotNone(self.analysis.dropoff_coordinates)
+        self.assertIsNotNone(self.analysis.pickup_address)
+        self.assertIsNotNone(self.analysis.dropoff_address)
 
-        self.assertIsNotNone(pickup_address)
-        self.assertIsNotNone(dropoff_address)
-
-    def test_get_addresses(self):
-        # Test that the returned address matches the expected address
-        pickup_address, dropoff_address = get_addresses(
-            [("40.77887344360352", "-73.95391845703125")],
-            [("40.77116394042969", "-73.96387481689453")],
-        )
-        pickup_expected_address = [
-            "1530, 3rd Avenue, Manhattan Community Board 8, "
-            "Manhattan, New York County, City of New York, New York, 10028, United States"
-        ]
-        dropoff_expected_address = [
-            "760, Park Avenue, Manhattan Community Board 8, "
-            "Manhattan, New York County, City of New York, New York, 11206, United States"
-        ]
-
-        self.assertEqual(pickup_address, pickup_expected_address)
-        self.assertEqual(dropoff_address, dropoff_expected_address)
+        self.assertEqual(addresses.loc[0, "pickup_address"],
+                         'Spectrum, 261, 3rd Avenue, Manhattan Community Board 6, '
+                         'Manhattan, New York County, City of New York, New York, 10010, United States')
+        self.assertEqual(addresses.loc[0, "dropoff_address"],
+                         'Pennsylvania Station, Lincoln Tunnel Expressway, Chelsea District, '
+                         'Manhattan, New York County, City of New York, New York, 10119, United States')
+        self.assertEqual(addresses.loc[2, "pickup_address"],
+                         '251, Church Street, Manhattan Community Board 1, '
+                         'Manhattan, New York County, City of New York, New York, 10013, United States')
+        self.assertEqual(addresses.loc[2, "dropoff_address"],
+                         '258, West 4th Street, Manhattan Community Board 2, '
+                         'Manhattan, New York County, City of New York, New York, 10014, United States')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
